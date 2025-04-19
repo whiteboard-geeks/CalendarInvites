649f03ac9cd25cac52b846e1ca7f15cd0986fa31
Lance Johnson <lancejohnson728@gmail.com>
2025-04-19 07:12:00 -0500

    Add test leads for April

--- close_reset.py
+++ close_reset.py
@@ -16,13 +16,19 @@ headers = {
 
 # List of lead IDs
 lead_ids = [
-    "lead_Lhqoxq3zAEdZKzGNPE6GuMW6V7mlMJlhQPU3R19vltp",  # CT - GMT-6
-    "lead_Z7wS8MX7wIcKcsgk6jgShfPYCyIpQct15sAAPtVYkFn",  # PT - GMT-8
-    "lead_NazuF7xufuyBEfxbSVj3Oy7O7XFUhuhKBbMw8gFnz0m",  # MT - GMT-7
-    "lead_Y3NyDeiRSY9HcgT7LqyrWzcvAuWrb0YQAeH6QQMqPrG",  # ET - GMT-5
-    "lead_CF7NLlmG3yircJjsRSzeOME2iXEY6W35Mam5gHNTdJ0",  # ET - GMT-5
-    "lead_Lcyyy03fdeMsBxMYQV0TxGMDPUYqryoS9zXsW6crB6O",  # HI - GMT-10
-    "lead_FcPBliF5o7VgaDA1q4odf6hqO3d22T7V9logAt8ysRj",  # AK - GMT-9
+    "lead_Lhqoxq3zAEdZKzGNPE6GuMW6V7mlMJlhQPU3R19vltp",  # BP - CT - GMT-6
+    "lead_Z7wS8MX7wIcKcsgk6jgShfPYCyIpQct15sAAPtVYkFn",  # BP - PT - GMT-8
+    "lead_NazuF7xufuyBEfxbSVj3Oy7O7XFUhuhKBbMw8gFnz0m",  # BP - MT - GMT-7
+    "lead_Y3NyDeiRSY9HcgT7LqyrWzcvAuWrb0YQAeH6QQMqPrG",  # BP - ET - GMT-5
+    "lead_CF7NLlmG3yircJjsRSzeOME2iXEY6W35Mam5gHNTdJ0",  # BP - ET - GMT-5
+    "lead_Lcyyy03fdeMsBxMYQV0TxGMDPUYqryoS9zXsW6crB6O",  # BP - HI - GMT-10
+    "lead_FcPBliF5o7VgaDA1q4odf6hqO3d22T7V9logAt8ysRj",  # BP - AK - GMT-9
+    "lead_LdcRg9QQqqseacmFqyIDmPMZkoJhH7N8vtXwjKI5OLz",  # AL - ET - GMT-5
+    "lead_ByjVODHgM5p87lxArQzB5JkkRvL833xhOsBdHXUxTc9",  # AL - CT - GMT-6
+    "lead_rIPIKEA2jTrDfXxk6IKxnu0Rs72g17KQA1n1Lf41n6F",  # AL - MT - GMT-7
+    "lead_EoogswUZmjMBbeAcdogZPvubHzd8mDlDveXUZ9Syp2q",  # AL - PT - GMT-8
+    "lead_n2UE7lOoSumV9gHEwp5B06IBqArxsuDrunbc3GrLRqo",  # AL - AK - GMT-9
+    "lead_ACHXiiNdBxTD1CrXpyCCmzCV0Gx5YBo00pAtbKvm89w",  # AL - HI - GMT-10
 ]
 
 # Task details
